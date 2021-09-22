bd8950dd1ac3de93048a233024c0bdc64030afdc
TheNeuron-EM <60265838+TheNeuron-EM@users.noreply.github.com>
2021-09-23 00:18:14 +0400

    Update constants.py

--- pipeline/constants.py
+++ pipeline/constants.py
@@ -6,4 +6,4 @@ IMG_SIZE = (224, 224)
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
 DEFAULT_DATA_PATH = os.path.join(PROJECT_PATH, 'data')
-DEFAULT_EXPERIMENTS_SAVE_PATH = os.path.join(PROJECT_PATH, 'experiments')
+DEFAULT_EXPERIMENTS_SAVE_PATH = os.path.join('/content/drive/MyDrive, 'experiments')
