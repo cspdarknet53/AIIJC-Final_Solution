cca86e5fa67f0bf198057abaf7fc1ea992a3ea6c
cspdarknet53 <91949209+cspdarknet53@users.noreply.github.com>
2021-11-14 21:26:15 +0400

    Update models.py

--- Solution Source (Source Code)/pipeline/models.py
+++ Solution Source (Source Code)/pipeline/models.py
@@ -10,16 +10,16 @@ from efficientnet_pytorch import EfficientNet
 ENCODERS = {
     
     'efficientnet': {
-        'features': 1408,
-        'init_op': EfficientNet.from_pretrained('efficientnet-b2', num_classes=131),
+        'features': 704,
+        'init_op': EfficientNet.from_pretrained('efficientnet-b2',
     },
     'efficientnetb7': {
         'features': 1408,
-        'init_op': EfficientNet.from_pretrained('efficientnet-b7', num_classes=131),
+        'init_op': EfficientNet.from_pretrained('efficientnet-b7'),
     },
     'efficientnetl2': {
         'features': 1408,
-        'init_op' : EfficientNet.from_name('efficientnet-l2', num_classes=131)
+        'init_op' : EfficientNet.from_name('efficientnet-l2')
     }
 }
 
@@ -40,8 +40,17 @@ class SignsClassifier(nn.Module):
         self.encoder = ENCODERS[encoder_name]['init_op']
         self.avg_pool = AdaptiveAvgPool2d((1, 1))
         self.dropout = Dropout(dropout_rate)
-        self.fc = Linear(ENCODERS[encoder_name]['features'], n_classes)
-
+        self.cv1 = nn.Conv2D(1408,704,3)
+        self.cv2 = nn.Conv2D(1408,704,3)
+        self.cv3 = nn.Conv2D(1408,704,3)
+        self.cv4 = nn.Conv2D(1408,704,3)
+        self.cv5 = nn.Conv2D(1408,704,3)
+        self.fc = Linear(ENCODERS[encoder_name]['features'], 5)
+        self.fc1 = Linear(ENCODERS[encoder_name]['features'], 6)
+        self.fc2 = Linear(ENCODERS[encoder_name]['features'], 6)
+        self.fc3 = Linear(ENCODERS[encoder_name]['features'], 6)
+        self.fc4 = Linear(ENCODERS[encoder_name]['features'], 6)
+        self.fc5 = Linear(ENCODERS[encoder_name]['features'], 6)
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Getting the model prediction.
 
@@ -49,5 +58,27 @@ class SignsClassifier(nn.Module):
         :return: prediction
         """
         x = self.encoder(inputs=x)
-        
-        return x
+        x1 = self.avg_pool(x)
+        x1 = nn.Softmax(self.fc(x1),dim=-1)
+
+        x2 = nn.ReLU(self.cv1(x))
+        x2 = self.avg_pool(x2)
+        x2 = nn.Sigmoid(self.fc1(x2),dim=-1)
+
+        x3 = nn.ReLU(self.cv2(x))
+        x3 = self.avg_pool(x3)
+        x3 = nn.Sigmoid(self.fc2(x3),dim=-1)
+
+        x4 = nn.ReLU(self.cv3(x))
+        x4 = self.avg_pool(x4)
+        x4 = nn.Sigmoid(self.fc3(x4),dim=-1)
+
+        x5 = nn.ReLU(self.cv4(x))
+        x5 = self.avg_pool(x5)
+        x5 = nn.Sigmoid(self.fc4(x5),dim=-1)
+
+        x6 = nn.ReLU(self.cv5(x))
+        x6 = self.avg_pool(x6)
+        x6 = nn.Sigmoid(self.fc5(x6),dim=-1) 
+
+        return x1,x2,x3,x4,x5,x6
